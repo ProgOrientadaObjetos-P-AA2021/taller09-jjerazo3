91917cea0eac8b67f1ef100b18fed20a90258071
jjerazo3 <73542938+jjerazo3@users.noreply.github.com>
2021-06-10 21:43:45 -0500

    Update Ejecutor.java

--- ManejoHerencia/src/paquete1/Ejecutor.java
+++ ManejoHerencia/src/paquete1/Ejecutor.java
@@ -3,6 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+// Jonathan Erazo Borrero
+// Jamil Quituizaca
 package paquete1;
 
 import java.util.Scanner;
